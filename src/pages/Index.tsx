9836fe4a108052c17acc0cdd6ee98790e6fe4299
samicowest <samicowest@gmail.com>
2025-06-30 04:08:23 -0700

    fix errors

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Navigation from "../components/layout/Navigation";
 import Footer from "../components/layout/Footer";
@@ -11,20 +10,6 @@ import TeamSection from "../components/TeamSection";
 import FAQSection from "../components/FAQSection";
 import NewsletterSection from "../components/NewsletterSection";
 import Whatsaap from "@/utils/Whatsaap";
-=======
-
-import React from 'react';
-import Navigation from '../components/layout/Navigation';
-import Footer from '../components/layout/Footer';
-import HeroSection from '../components/sections/HeroSection';
-import ServicesSection from '../components/sections/ServicesSection';
-import AboutSection from '../components/AboutSection';
-import ResearchSection from '../components/sections/ResearchSection';
-import VenturesSection from '../components/VenturesSection';
-import TeamSection from '../components/TeamSection';
-import FAQSection from '../components/FAQSection';
-import NewsletterSection from '../components/NewsletterSection';
->>>>>>> 911fb5c6cb0d2be1ca7dd223dcae5737079af8ed
 
 const Index = () => {
   return (
@@ -39,10 +24,7 @@ const Index = () => {
       <FAQSection />
       <NewsletterSection />
       <Footer />
-<<<<<<< HEAD
       <Whatsaap />
-=======
->>>>>>> 911fb5c6cb0d2be1ca7dd223dcae5737079af8ed
     </div>
   );
 };
